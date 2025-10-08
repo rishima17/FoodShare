d441626e7dff318b0545a94c0f6ea3f2b13277cb
Rishima <rishima1711@gmail.com>
2025-10-08 10:39:54 +0530

    added backend deployed url

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -32,7 +32,7 @@ const ContactUs = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:3000/api/contact", {
+      const res = await fetch("https://leftover-food-donation.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
